ece78c557e7143040d2a64d380591f5f71a8c684
Phuc <letanphuc123@gmail.com>
2015-05-12 17:11:29 +0700

    Change new source.

---
+++ Source/CalculatorTypeScript/Script/myTypeScript.js
@@ -0,0 +1,16 @@
+// main class
+var Calculator = (function () {
+    function Calculator() {
+    }
+    Calculator.prototype.getResultOperation = function () {
+    };
+    return Calculator;
+})();
+// implement function
+window.onload = function () {
+    $("#resultValue").val("0");
+    $(".calbtnumber").click(function () {
+        //$("#resultValue").val(string => );
+    });
+};
+//# sourceMappingURL=myTypeScript.js.map
---
+++ Source/CalculatorTypeScript/Script/myTypeScript.ts
@@ -0,0 +1,24 @@
+﻿// main class
+class Calculator implements IResultFunction {
+	getResultOperation() {
+
+	}
+}
+
+// main interface
+interface IResultInformation {
+	result: string;
+	resultToken: string;
+}
+
+interface IResultFunction {
+	getResultOperation();
+}
+
+// implement function
+window.onload = function () {
+	$("#resultValue").val("0");
+	$(".calbtnumber").click(function () {
+		//$("#resultValue").val(string => );
+	});
+}
