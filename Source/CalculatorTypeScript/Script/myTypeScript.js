06c7cb61ed606043bfda403cdf2857dd83c2c46e
Phuc <letanphuc123@gmail.com>
2015-05-13 17:01:34 +0700

    Update source.

--- Source/CalculatorTypeScript/Script/myTypeScript.js
+++ Source/CalculatorTypeScript/Script/myTypeScript.js
@@ -1,16 +1,35 @@
 // main class
 var Calculator = (function () {
-    function Calculator() {
+    // constructor
+    function Calculator(outputString, newNumber, flag) {
+        this._outputString = outputString;
+        this._newNumber = newNumber;
+        this._flag = flag;
     }
+    // functions
     Calculator.prototype.getResultOperation = function () {
+    };
+    Calculator.prototype.getResultOutput = function (btn) {
+        return btn;
     };
     return Calculator;
 })();
+// enum
+var ConstantVariable;
+(function (ConstantVariable) {
+    ConstantVariable[ConstantVariable["add"] = 1] = "add";
+    ConstantVariable[ConstantVariable["sub"] = 2] = "sub";
+    ConstantVariable[ConstantVariable["div"] = 3] = "div";
+    ConstantVariable[ConstantVariable["mult"] = 4] = "mult";
+})(ConstantVariable || (ConstantVariable = {}));
 // implement function
 window.onload = function () {
+    var resultInformation = { _outputString: "0", _newNumber: true, _flag: null };
+    var cal = new Calculator(resultInformation._outputString, resultInformation._newNumber, resultInformation._flag);
     $("#resultValue").val("0");
     $(".calbtnumber").click(function () {
-        //$("#resultValue").val(string => );
+        var _this = this;
+        $("#resultValue").val(function (string) { return cal.getResultOutput($(_this).val()); });
     });
 };
 //# sourceMappingURL=myTypeScript.js.map
--- Source/CalculatorTypeScript/Script/myTypeScript.ts
+++ Source/CalculatorTypeScript/Script/myTypeScript.ts
@@ -1,24 +1,62 @@
 ﻿// main class
-class Calculator implements IResultFunction {
+class Calculator implements IResultFunction, IResultInformation {
+	// properties
+	_outputString: string;
+	_newNumber: boolean;
+	_flag: string;
+	_result: string;
+	_resultToken: string;
+	// constructor
+	constructor(outputString: string, newNumber: boolean, flag: string) {
+		this._outputString = outputString;
+		this._newNumber = newNumber;
+		this._flag = flag;
+	}
+
+	// functions
 	getResultOperation() {
 
 	}
+
+	getResultOutput(btn: string) {
+		return btn;
+	}
+}
+
+// enum
+enum ConstantVariable {
+	add = 1,
+	sub = 2,
+	div = 3,
+	mult = 4
 }
 
 // main interface
-interface IResultInformation {
-	result: string;
-	resultToken: string;
+interface IResultInformation extends IBaseVariable {
+	_result: string;
+	_resultToken: string;
 }
 
 interface IResultFunction {
-	getResultOperation();
+	getResultOperation(): any;
+	getResultOutput(btn: string): any;
+}
+
+interface IBaseVariable {
+	// properties
+	_outputString: string;
+	_newNumber: boolean;
+
+	// constant;
+	_flag: string;
 }
 
 // implement function
 window.onload = function () {
+	var resultInformation = <IResultInformation>{_outputString: "0", _newNumber: true, _flag: null};
+	var cal = new Calculator(resultInformation._outputString, resultInformation._newNumber, resultInformation._flag);
 	$("#resultValue").val("0");
 	$(".calbtnumber").click(function () {
-		//$("#resultValue").val(string => );
+		$("#resultValue").val(string => cal.getResultOutput($(this).val()));
 	});
 }
