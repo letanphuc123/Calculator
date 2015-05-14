9fa1492cf58c2a75d7ceb250afdd18ebdc5ad41d
Phuc <letanphuc123@gmail.com>
2015-05-14 08:50:41 +0700

    Update source to review.

--- Source/CalculatorTypeScript/Script/myTypeScript.js
+++ Source/CalculatorTypeScript/Script/myTypeScript.js
@@ -1,35 +1,111 @@
+/// <reference path="../scripts/typings/jquery/jquery.d.ts" />
 // main class
 var Calculator = (function () {
     // constructor
-    function Calculator(outputString, newNumber, flag) {
+    function Calculator(outputString, newNumber, flag, result, resultToken) {
         this._outputString = outputString;
         this._newNumber = newNumber;
         this._flag = flag;
+        this._result = result;
+        this._resultToken = resultToken;
+        this._outputTotal = 0;
     }
     // functions
-    Calculator.prototype.getResultOperation = function () {
+    Calculator.prototype.getResultOperation = function (operation) {
+        switch (operation) {
+            case 0 /* add */:
+                this._resultToken += this._result + " " + "+" + "  ";
+                this._flag = 1 /* isPlus */;
+                this._outputTotal += this._result;
+                break;
+            case 1 /* sub */:
+                this._resultToken += this._result + " " + "-" + "  ";
+                this._flag = 2 /* isSub */;
+                this._outputTotal -= this._result;
+                break;
+            case 2 /* div */:
+                this._resultToken += this._result + " " + "/" + "  ";
+                this._flag = 3 /* isDiv */;
+                this._outputTotal = this._outputTotal / this._result;
+                break;
+                ;
+            case 3 /* mult */:
+                this._resultToken += this._result + " " + "*" + "  ";
+                this._flag = 4 /* isMul */;
+                this._outputTotal = this._outputTotal * this._result;
+                break;
+        }
+        var resultOperation = { result: this._outputTotal, resultToken: this._resultToken };
+        return resultOperation;
     };
     Calculator.prototype.getResultOutput = function (btn) {
+        this._result = parseFloat(btn);
+        this._flag = 0 /* isNumber */;
         return btn;
+    };
+    Calculator.prototype.clearResult = function () {
+        this._flag = 0 /* isNumber */;
+        this._newNumber = true;
+        this._outputString = "0";
+        this._outputTotal = 0;
+        this._result = 0;
+        this._resultToken = "";
+        var resultOperation = { result: this._outputTotal, resultToken: this._resultToken };
+        return resultOperation;
     };
     return Calculator;
 })();
 // enum
 var ConstantVariable;
 (function (ConstantVariable) {
-    ConstantVariable[ConstantVariable["add"] = 1] = "add";
-    ConstantVariable[ConstantVariable["sub"] = 2] = "sub";
-    ConstantVariable[ConstantVariable["div"] = 3] = "div";
-    ConstantVariable[ConstantVariable["mult"] = 4] = "mult";
+    ConstantVariable[ConstantVariable["add"] = 0] = "add";
+    ConstantVariable[ConstantVariable["sub"] = 1] = "sub";
+    ConstantVariable[ConstantVariable["div"] = 2] = "div";
+    ConstantVariable[ConstantVariable["mult"] = 3] = "mult";
 })(ConstantVariable || (ConstantVariable = {}));
+;
+var ConstantFlag;
+(function (ConstantFlag) {
+    ConstantFlag[ConstantFlag["isNumber"] = 0] = "isNumber";
+    ConstantFlag[ConstantFlag["isPlus"] = 1] = "isPlus";
+    ConstantFlag[ConstantFlag["isSub"] = 2] = "isSub";
+    ConstantFlag[ConstantFlag["isDiv"] = 3] = "isDiv";
+    ConstantFlag[ConstantFlag["isMul"] = 4] = "isMul";
+})(ConstantFlag || (ConstantFlag = {}));
+;
 // implement function
 window.onload = function () {
-    var resultInformation = { _outputString: "0", _newNumber: true, _flag: null };
-    var cal = new Calculator(resultInformation._outputString, resultInformation._newNumber, resultInformation._flag);
-    $("#resultValue").val("0");
+    var resultInformation = { _outputString: "0", _newNumber: true, _flag: 0 /* isNumber */, _result: 0, _resultToken: "" };
+    var cal = new Calculator(resultInformation._outputString, resultInformation._newNumber, resultInformation._flag, resultInformation._result, resultInformation._resultToken);
+    $("#resultValue").val(cal._result.toString());
     $(".calbtnumber").click(function () {
         var _this = this;
         $("#resultValue").val(function (string) { return cal.getResultOutput($(_this).val()); });
+    });
+    $("#calPlus").click(function () {
+        var resultOperation = cal.getResultOperation(0 /* add */);
+        $("#resultValue").val(function (string) { return resultOperation.result.toString(); });
+        $("#resultToken").text(function (string) { return resultOperation.resultToken; });
+    });
+    $("#calSub").click(function () {
+        var resultOperation = cal.getResultOperation(1 /* sub */);
+        $("#resultValue").val(function (string) { return resultOperation.result.toString(); });
+        $("#resultToken").text(function (string) { return resultOperation.resultToken; });
+    });
+    $("#calDiv").click(function () {
+        var resultOperation = cal.getResultOperation(2 /* div */);
+        $("#resultValue").val(function (string) { return resultOperation.result.toString(); });
+        $("#resultToken").text(function (string) { return resultOperation.resultToken; });
+    });
+    $("#calMul").click(function () {
+        var resultOperation = cal.getResultOperation(3 /* mult */);
+        $("#resultValue").val(function (string) { return resultOperation.result.toString(); });
+        $("#resultToken").text(function (string) { return resultOperation.resultToken; });
+    });
+    $("#calClear").click(function () {
+        var resultOperation = cal.clearResult();
+        $("#resultValue").val(function (string) { return resultOperation.result.toString(); });
+        $("#resultToken").text(function (string) { return resultOperation.resultToken; });
     });
 };
 //# sourceMappingURL=myTypeScript.js.map
--- Source/CalculatorTypeScript/Script/myTypeScript.ts
+++ Source/CalculatorTypeScript/Script/myTypeScript.ts
@@ -1,44 +1,81 @@
-﻿// main class
+﻿/// <reference path="../scripts/typings/jquery/jquery.d.ts" />
+// main class
 class Calculator implements IResultFunction, IResultInformation {
 	// properties
 	_outputString: string;
+	_outputTotal: number;
 	_newNumber: boolean;
-	_flag: string;
-	_result: string;
+	_flag: number;
+	_result: number;
 	_resultToken: string;
 	// constructor
-	constructor(outputString: string, newNumber: boolean, flag: string) {
+	constructor(outputString: string, newNumber: boolean, flag: number, result: number, resultToken: string) {
 		this._outputString = outputString;
 		this._newNumber = newNumber;
 		this._flag = flag;
+		this._result = result;
+		this._resultToken = resultToken;
+		this._outputTotal = 0;
 	}
 
 	// functions
-	getResultOperation() {
-
+	getResultOperation(operation: number) {
+		switch (operation) {
+			case ConstantVariable.add:
+				this._resultToken += this._result + " " + "+" + "  ";
+				this._flag = ConstantFlag.isPlus;
+				this._outputTotal += this._result;
+				break;
+			case ConstantVariable.sub:
+				this._resultToken += this._result + " " + "-" + "  ";
+				this._flag = ConstantFlag.isSub;
+				this._outputTotal -= this._result;
+				break;
+			case ConstantVariable.div:
+				this._resultToken += this._result + " " + "/" + "  ";
+				this._flag = ConstantFlag.isDiv;
+				this._outputTotal = this._outputTotal / this._result;
+				break;;
+			case ConstantVariable.mult:
+				this._resultToken += this._result + " " + "*" + "  ";
+				this._flag = ConstantFlag.isMul;
+				this._outputTotal = this._outputTotal * this._result;
+				break;
+		}
+		var resultOperation = { result: this._outputTotal, resultToken: this._resultToken };
+		return resultOperation;
 	}
 
 	getResultOutput(btn: string) {
+		this._result = parseFloat(btn);
+		this._flag = ConstantFlag.isNumber;
 		return btn;
+	}
+
+	clearResult() {
+		this._flag = ConstantFlag.isNumber;
+		this._newNumber = true;
+		this._outputString = "0";
+		this._outputTotal = 0;
+		this._result = 0;
+		this._resultToken = "";
+		var resultOperation = { result: this._outputTotal, resultToken: this._resultToken };
+		return resultOperation;
 	}
 }
 
 // enum
-enum ConstantVariable {
-	add = 1,
-	sub = 2,
-	div = 3,
-	mult = 4
-}
+enum ConstantVariable { add, sub, div, mult };
+enum ConstantFlag { isNumber, isPlus, isSub, isDiv, isMul };
 
 // main interface
 interface IResultInformation extends IBaseVariable {
-	_result: string;
+	_result: number;
 	_resultToken: string;
 }
 
 interface IResultFunction {
-	getResultOperation(): any;
+	getResultOperation(operation: number): any;
 	getResultOutput(btn: string): any;
 }
 
@@ -48,15 +85,44 @@ interface IBaseVariable {
 	_newNumber: boolean;
 
 	// constant;
-	_flag: string;
+	_flag: number;
 }
 
 // implement function
 window.onload = function () {
-	var resultInformation = <IResultInformation>{_outputString: "0", _newNumber: true, _flag: null};
-	var cal = new Calculator(resultInformation._outputString, resultInformation._newNumber, resultInformation._flag);
-	$("#resultValue").val("0");
+	var resultInformation = <IResultInformation>{ _outputString: "0", _newNumber: true, _flag: ConstantFlag.isNumber, _result: 0, _resultToken: "" };
+	var cal = new Calculator(resultInformation._outputString, resultInformation._newNumber, resultInformation._flag, resultInformation._result, resultInformation._resultToken);
+	$("#resultValue").val(cal._result.toString());
 	$(".calbtnumber").click(function () {
 		$("#resultValue").val(string => cal.getResultOutput($(this).val()));
+	});
+
+	$("#calPlus").click(function () {
+		var resultOperation = cal.getResultOperation(ConstantVariable.add);
+		$("#resultValue").val(string => resultOperation.result.toString());
+		$("#resultToken").text(string => resultOperation.resultToken);
+	});
+	$("#calSub").click(function () {
+		var resultOperation = cal.getResultOperation(ConstantVariable.sub);
+		$("#resultValue").val(string => resultOperation.result.toString());
+		$("#resultToken").text(string => resultOperation.resultToken);
+	});
+
+	$("#calDiv").click(function () {
+		var resultOperation = cal.getResultOperation(ConstantVariable.div);
+		$("#resultValue").val(string => resultOperation.result.toString());
+		$("#resultToken").text(string => resultOperation.resultToken);
+	});
+
+	$("#calMul").click(function () {
+		var resultOperation = cal.getResultOperation(ConstantVariable.mult);
+		$("#resultValue").val(string => resultOperation.result.toString());
+		$("#resultToken").text(string => resultOperation.resultToken);
+	});
+
+	$("#calClear").click(function () {
+		var resultOperation = cal.clearResult();
+		$("#resultValue").val(string => resultOperation.result.toString());
+		$("#resultToken").text(string => resultOperation.resultToken);
 	});
 }
